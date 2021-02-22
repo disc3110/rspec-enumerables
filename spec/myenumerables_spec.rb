06b6efbfb8ff42ec06ec03bbca60f8c04fb82609
Diego Solis <disc3110@gmail.com>
2021-02-22 14:13:10 -0600

    set rspec

---
+++ spec/myenumerables_spec.rb
@@ -0,0 +1,32 @@
+require_relative '../myenumerables_disc.rb'
+
+describe Enumerable do
+  let(:integer_array) {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]}
+  describe '#my_each' do
+
+  end
+  describe '#my_each_with_index' do
+
+  end
+  describe '#my_select' do
+
+  end
+  describe '#my_all?' do
+
+  end
+  describe '#my_any' do
+
+  end
+  describe '#my_none' do
+
+  end
+  describe '#my_count' do
+
+  end
+  describe '#my_map' do
+
+  end
+  describe '#my_inject' do
+    
+  end
+end
