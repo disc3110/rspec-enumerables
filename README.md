8ee902620577d367f5ca24705ff9ffc1df36f463
Diego Solis <disc3110@gmail.com>
2021-02-22 13:55:31 -0600

    make rspec copy

--- README.md
+++ README.md
@@ -1,2 +1,71 @@
-# rspec-enumerables
- 
+![](https://img.shields.io/badge/Microverse-blueviolet)
+
+# Project Name: RUBY - My Enumerables
+
+Implementing enumerables in Ruby comparable to the existing enumerable methods.
+IMPLEMENTED METHODS
+#### IMPLEMENTED METHODS
+1. Create #my_each, a method that is identical to #each but (obviously) does not use #each. You'll need to remember the yield statement. Make sure it returns the same thing as #each as well.
+2. Create #my_each_with_index in the same way.
+3. Create #my_select in the same way, though you may use #my_each in your definition (but not #each).
+4. Create #my_all? (continue as above)
+5. Create #my_any?
+6. Create #my_none?
+7. Create #my_count
+8. Create #my_map
+9. Create #my_inject
+10. Test your #my_inject by creating a method called #multiply_els which multiplies all the elements of the array together by using #my_inject, e.g. multiply_els([2,4,5]) #=> 40
+11. Modify your #my_map method to take a proc instead.
+12. Modify your #my_map method to take either a proc or a block. It won't be necessary to apply both a proc and a block in the same #my_map call since you could get the same effect by chaining together one #my_map call with the block and one with the proc. This approach is also clearer since the user doesn't have to remember whether the proc or block will be run first. So if both a proc and a block are given, only execute the proc.
+
+## Skills Revived & Learned:
+
+- Versioning & best coding practices, Git Flow.
+- RUBY 3.0, various tools and features etc.
+- Linter - install & fix errors
+- Collaborative learning, being patient, helping each other succeed.
+- Self Monitoring & reliable project deliverables on due time, being focused, accountable.
+- Ability to work independently and ask for help after due self research when needed.
+
+### Learning Goals:
+
+- Version Control, Git Flow
+- RUBY Core, Enumerables, Methods, Yield, Blocks
+
+## Built With
+
+- Major languages: RUBY 3
+- Frameworks: Git flow
+
+## Steps to Clone/Customize the Website Template
+
+- Download/Clone/Fork the Repo
+- Install Ruby 3.0, Gems
+
+## Authors
+
+👤 **Shabbir**
+
+- GitHub: [@smy5152](https://github.com/smy5152)
+- Twitter: [@smy5152](https://twitter.com/smy5152)
+- LinkedIn: [Shabbir Yamani](https://www.linkedin.com/in/shabbirmyamani/)
+
+👤 **Diego**
+
+- GitHub: [@disc3110](https://github.com/disc3110)
+- Twitter: [@disc3110](https://twitter.com/disc3110)
+- LinkedIn: [@diego-solis-277651184](https://www.linkedin.com/in/diego-solis-277651184/)
+
+## Show your support
+
+Give a ⭐️ if you like this project!
+
+## Acknowledgments
+
+- Hat tip to anyone whose code was used
+- Inspiration
+- Learning
+
+## 📝 License
+
+This project is [MIT](./LICENSE) licensed.
---
+++ myenumerables_disc.rb
@@ -0,0 +1,192 @@
+# rubocop:disable Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity, Metrics/MethodLength, Metrics/ModuleLength
+
+# MY ENUMERABLES
+module Enumerable
+  # each
+  def my_each(*)
+    return enum_for(:my_each) unless block_given?
+
+    # my_each(&block)
+    arr = self if instance_of?(Array)
+    arr = to_a if instance_of?(Range)
+    i = 0
+    while i < arr.length
+      yield(arr[i])
+      i += 1
+    end
+    self
+  end
+
+  # my_each_with_index
+  def my_each_with_index
+    return enum_for(:my_each) unless block_given?
+
+    myindex = 0
+    my_each do |i|
+      yield i, myindex
+      myindex += 1
+    end
+    self
+  end
+
+  # my_select
+  def my_select
+    return enum_for(:my_each) unless block_given?
+
+    selected = []
+    my_each do |i|
+      selected.push(i) if yield i
+    end
+    selected
+  end
+
+  # my_all? ------------
+  def my_all?(parameter = nil)
+    my_each do |x|
+      if block_given?
+        return false unless yield x
+      elsif parameter.is_a?(Regexp)
+        return false unless parameter.match?(x)
+      elsif parameter.is_a?(Class)
+        return false unless x.is_a?(parameter)
+      elsif parameter.nil?
+        return false unless x
+      else
+        return false unless x == parameter
+      end
+    end
+    true
+  end
+
+  # my_any?
+  def my_any?(parameter = nil)
+    my_each do |x|
+      if block_given?
+        return true if yield x
+      elsif parameter.is_a?(Class)
+        return true if x.is_a?(parameter)
+      elsif parameter.is_a?(Regexp)
+        return true if parameter.match?(x)
+      elsif parameter.nil?
+        return true if x
+      elsif x == parameter
+        return true
+      end
+    end
+    false
+  end
+
+  # my_none?
+  def my_none?(parameter = nil)
+    my_each do |x|
+      if block_given?
+        return false if yield x
+      elsif parameter.is_a?(Class)
+        return false if x.is_a?(parameter)
+      elsif parameter.is_a?(Regexp)
+        return false if parameter.match?(x)
+      elsif parameter.nil?
+        return false if x
+      elsif x == parameter
+        return false
+      end
+    end
+    true
+  end
+
+  # my_count
+  def my_count(parameter = nil)
+    res = 0
+    my_each do |x|
+      if block_given?
+        res += 1 if yield x
+      elsif parameter.nil?
+        res += 1
+      elsif parameter == x
+        res += 1
+      end
+    end
+    res
+  end
+
+  # my_map
+  def my_map(param = nil)
+    return to_enum(:my_map) unless block_given? || param
+
+    retarr = []
+    my_each do |x|
+      if param.is_a?(Proc)
+        retarr.push(param.call(x))
+      else
+        retarr.push(yield x)
+      end
+    end
+    retarr
+  end
+
+  # my_inject
+  def my_inject(initial = nil, sym = nil)
+    raise LocalJumpError if initial.nil? && sym.nil? && !block_given?
+
+    if initial.is_a? Symbol
+      sym = initial
+      initial = nil
+    end
+    if is_a? Array
+      if block_given?
+        accum = if initial.nil?
+                  self[0]
+                else
+                  yield(initial, self[0])
+                end
+      end
+      unless sym.nil?
+        accum = if initial.nil?
+                  self[0]
+                else
+                  initial.public_send(sym, self[0])
+                end
+      end
+
+      x = 1
+      while x < length
+        accum = accum.public_send(sym, self[x]) unless sym.nil?
+        accum = yield accum, self[x] if block_given?
+        x += 1
+      end
+    end
+    if is_a? Range
+      if block_given?
+        accum = if initial.nil?
+                  self.begin
+                else
+                  yield(initial, self.begin)
+                end
+      end
+      unless sym.nil?
+        accum = if initial.nil?
+                  self.begin
+                else
+                  initial.public_send(sym, self.begin)
+                end
+      end
+
+      x = Range.new(self.begin + 1, self.end)
+      x.each do |i|
+        accum = accum.public_send(sym, i) unless sym.nil?
+        accum = yield accum, i if block_given?
+      end
+    end
+    accum
+  end
+end
+
+def multiply_els(arr)
+  # Can Provide the BLOCK
+  out = (arr.my_inject { |result, element| (result * element) })
+  p out
+end
+numbers = [2, 4, 5]
+multiply_els(numbers)
+
+# rubocop:enable Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity, Metrics/MethodLength, Metrics/ModuleLength
